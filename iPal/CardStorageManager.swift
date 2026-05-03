4d5325b854a3d5d70fe7c96e0456fb1c7cb9ff00
Ranu <ranu13goyal@gmail.com>
2026-05-03 09:35:41 +0530

    feat: add CardStorageManager for local card persistence

---
+++ iPal/CardStorageManager.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+final class CardStorageManager {
+    static let shared = CardStorageManager()
+    private let fileName = "buffered_cards.json"
+    
+    private var fileURL: URL {
+        FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+            .appendingPathComponent(fileName)
+    }
+    
+    private init() {}
+    
+    func getAllCards() -> [SummaryCard] {
+        guard let data = try? Data(contentsOf: fileURL),
+              let cards = try? JSONDecoder().decode([SummaryCard].self, from: data) else {
+            return []
+        }
+        return cards
+    }
+    
+    func saveCards(_ cards: [SummaryCard]) {
+        if let data = try? JSONEncoder().encode(cards) {
+            try? data.write(to: fileURL)
+        }
+    }
+    
+    func appendCard(_ card: SummaryCard) {
+        var current = getAllCards()
+        current.append(card)
+        saveCards(current)
+    }
+}
